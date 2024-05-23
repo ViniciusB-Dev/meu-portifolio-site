09c27e4a4ecc7bbd6a82228a21fd9c3dd7a1bab9
Vinicius Barbosa <ninho5@live.com>
2024-05-23 00:47:10 -0300

    atualizando e upando portifolio

---
+++ assets/js/js.js
@@ -0,0 +1,34 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("submitBtn").addEventListener("click", function(event) {
+        event.preventDefault();
+        const formData = new FormData(document.getElementById("discordForm"));
+        const params = {
+            name: formData.get("name"),
+            email: formData.get("email"),
+            subject: formData.get("subject"),
+            comment: formData.get("comment")
+        };
+
+        fetch('https://discordapp.com/api/webhooks/1242927556626485258/jKxlvnb6UXzQ7sSKhFf_CypfnuhX3J7qBx55kXkfK1KRlxiKwOJgQzg4Smg_Ksppq9Bf', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                content: `Novo envio do formulário:\n\nNome: ${params.name}\nEmail: ${params.email}\nTema: ${params.subject}\nMensagem: ${params.comment}\n`
+            }),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao enviar mensagem para o Discord');
+                }
+                alert('Mensagem enviada com sucesso para o Discord!');
+                document.getElementById("discordForm").reset(); // Limpar o formulário após o envio
+            })
+            .catch(error => {
+                console.error('Erro ao enviar mensagem:', error);
+                alert('Erro ao enviar mensagem para o Discord. Por favor, tente novamente mais tarde.');
+            });
+    });
+});
